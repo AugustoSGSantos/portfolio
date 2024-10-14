f819934dfd46a26db54f7a7c145e2e22d6d31a31
Augusto de Souza <augusto23sgs@gmail.com>
2024-10-14 13:27:56 -0300

    construindo estrutura e seção "sobre mim"

---
+++ src/containers/Sobre/index.tsx
@@ -0,0 +1,21 @@
+import Titulo from '../../components/Titulo'
+import Paragrafo from '../../components/Paragrafo'
+import { GithubSecao } from './styles'
+
+const Sobre = () => (
+  <section>
+    <Titulo fontSize={16}>Sobre mim</Titulo>
+    <Paragrafo tipo="secundario">
+      Lorem ipsum dolor sit, amet consectetur adipisicing elit. Hic ea
+      voluptatem sequi quidem aliquid id dolorem laborum dignissimos fuga
+      voluptates totam tempora nemo commodi, suscipit quaerat, ipsam consequatur
+      aut voluptas.
+    </Paragrafo>
+    <GithubSecao>
+      <img src="https://github-readme-stats.vercel.app/api?username=AugustoSGSantos&show_icons=true&theme=tokyonight&include_all_commits=true&count_private=true" />
+      <img src="https://github-readme-stats.vercel.app/api/top-langs/?username=AugustoSGSantos&layout=compact&langs_count=7&theme=tokyonight" />
+    </GithubSecao>
+  </section>
+)
+
+export default Sobre
